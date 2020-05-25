50baa4cdbf4fccef54ca0ecd0b0038cccca659f4
Gulshan Kumar Verma <33981412+scorpion231@users.noreply.github.com>
2020-05-25 13:46:29 +0530

    Hello world prog

---
+++ Mailing Address.py
@@ -0,0 +1,6 @@
+# Que 1 .
+# Create a program that displays your name and complete mailing address.
+# The addrtess should be printed in the formal that is normally used in the area where you live .
+# Your program does not need to read any input from the user
+
+print("hello world\nwhat is happening\nthis is a new apartment\nwhat the hell")
